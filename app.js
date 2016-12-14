9d6d5e0cd9a9e77e5658d8511d513c58dfc22109
igos_mmz <igos_mmz@fiberhome.com>
2016-12-14 10:32:49 +0800

    add extend React.Component class and use

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
+class StoryBox extends React.Component {
+  render() {
+    return(
+      <div>
+        <h3>Horse Story</h3>
+        <p className="lead">Some word</p>
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
-  <h1>Hello, horse</h1>,
+  <StoryBox />,
   document.getElementById('example')
 );
