cbd1ac6bdc8ee39c498713fe83e74f6a210268ef
igos_mmz <igos_mmz@fiberhome.com>
2016-12-14 10:51:32 +0800

    add Date() function and array's map function, it is common pattern to map arrays to JSXelements

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 class StoryBox extends React.Component {
   render() {
+
+    const now = new Date();
+    const topicsList = ['HTML', 'Javascript', 'React'];
     return(
+
       <div>
         <h3>Horse Story</h3>
-        <p className="lead">Some word</p>
+        <p className="lead">
+          Current time: {now.toTimeString()}
+        </p>
+        <ul>
+          {topicsList.map( topic => <li>{topic}</li> )}
+        </ul>
       </div>
     );
   }
